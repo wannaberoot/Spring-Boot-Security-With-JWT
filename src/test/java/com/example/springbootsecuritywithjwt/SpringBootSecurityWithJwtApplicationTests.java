9cd0c9dd86ac8d300438269e2c50556c621e581f
Ozan Sahin <kamilozan35@gmail.com>
2022-06-14 23:58:53 +0300

    Add main class, pom.xml and other Maven attributes

---
+++ src/test/java/com/example/springbootsecuritywithjwt/SpringBootSecurityWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecuritywithjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
